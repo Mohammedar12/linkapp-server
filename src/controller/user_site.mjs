3d201e36a5b99f2bc7f25a10305e2feff5d52d8a
Mohammedar12 <alikkar87@gmail.com>
2025-01-10 03:35:55 +0300

    new updates

--- src/controller/user_site.mjs
+++ src/controller/user_site.mjs
@@ -1,90 +1,68 @@
 import { UserSite } from "../models/user_site.mjs";
 import { User } from "../models/user.mjs";
 import { Links } from "../models/links.mjs";
-import { Header } from "../models/header.mjs";
 import { tryCatch } from "../utils/tryCatch.mjs";
 import { upload } from "../utils/cloudinary.mjs";
-import { ObjectId } from "mongodb";
+import mongoose from "mongoose";
 import fs from "fs";
 import "dotenv/config";
-import mongoose from "mongoose";
-
 import _ from "lodash";
 
 const handleFileUpload = async (file, dir) => {
   if (!file) return null;
 
-  try {
-    const { path } = file;
-    console.log("path", path);
-    const uploadedFile = await upload(path, `app/${dir}`);
-
-    await fs.unlinkSync(path);
-
-    return uploadedFile;
-  } catch (error) {
-    console.error("Error handling file upload:", error);
-    throw error;
-  }
+  const { path } = file;
+  const uploadedFile = await upload(path, `app/${dir}`);
+  await fs.unlinkSync(path);
+  return uploadedFile;
 };
 
 const UserSiteController = {
   index: tryCatch(async (req, res) => {
-    try {
-      const slug = req.params.slug;
+    const slug = req.params.slug;
 
-      const site = await UserSite.findOne({ slug }).populate({
-        path: "links",
-        match: { display: true },
+    const site = await UserSite.findOne({ slug }).populate({
+      path: "links",
+      match: { display: true },
+    });
+
+    if (!site) {
+      return res.json({
+        isActive: false,
+        message: "Sorry This Site Not Exist",
       });
-
-      if (!site) {
-        return res.json({
-          isActive: false,
-          message: "Sorry This Site Not Exist",
-        });
-      }
-
-      if (!site.isActive) {
-        return res.json({
-          isActive: site.isActive,
-          message: "the site not active yet",
-        });
-      }
-
-      res.json(site);
-    } catch (error) {
-      console.error("Error in index function:", error);
-      res.status(500).json({ message: "Internal server error" });
     }
+
+    if (!site.isActive) {
+      return res.json({
+        isActive: site.isActive,
+        message: "the site not active yet",
+      });
+    }
+
+    return res.json(site);
   }),
+
   id: tryCatch(async (req, res) => {
     const id = req.cookies["id"];
     const jwt = req.cookies["authenticated"];
     const site = await UserSite.findOne({ user: id }).populate("links");
 
-    if (jwt) {
-      console.log(jwt, "i'm jwt");
-    }
     if (!site) {
-      res.status(404).json({ error: "The site not exist" });
-      // throw new Error("The site not exist yet");
+      return res.status(404).json({ error: "The site not exist" });
     }
 
-    res.json(site);
+    return res.json(site);
   }),
+
   create: tryCatch(async (req, res) => {
     const { social, slug, title, theme, about, skills } = req.body;
     const id = req.cookies["id"];
     const user = await User.findById({ _id: id });
 
-    let avatar = null;
-
-    if (req.files) {
-      if (req.files.avatar) {
-        avatar = await handleFileUpload(req.files.avatar[0], "avatar");
-      }
-    }
+    const avatar = req.files?.avatar
+      ? await handleFileUpload(req.files.avatar[0], "avatar")
+      : null;
 
     const formattedSlug = slug?.replace(/\s+/g, "-").toLowerCase();
     const newSite = new UserSite({
@@ -100,10 +78,10 @@ const UserSiteController = {
     });
 
     await User.findOneAndUpdate({ _id: id }, { username: newSite.slug });
-
     const site = await newSite.save();
-    res.json(site);
+    return res.json(site);
   }),
+
   update: tryCatch(async (req, res) => {
     const {
       social,
@@ -117,43 +95,27 @@ const UserSiteController = {
     } = req.body;
     const id = req.cookies["id"];
     const io = req.app.get("io");
-    let avatar = null;
-    let bgImage = null;
-    let theme = null;
-
-    // Parse theme if it exists
-    if (req.body.theme) {
-      try {
-        theme = JSON.parse(req.body.theme);
-        console.log(theme);
-      } catch (e) {
-        console.error("Error parsing theme:", e);
-        return res.status(400).json({ message: "Invalid theme JSON" });
-      }
-    }
 
     // Handle file uploads
-    if (req.files) {
-      if (req.files.avatar) {
-        avatar = await handleFileUpload(req.files.avatar[0], "avatar");
-      }
-      if (req.files.bgImage) {
-        bgImage = await handleFileUpload(req.files.bgImage[0], "backgrounds");
-        if (theme) {
-          theme.bgImage = bgImage;
-        }
-      }
+    const avatar = req.files?.avatar
+      ? await handleFileUpload(req.files.avatar[0], "avatar")
+      : null;
+    const bgImage = req.files?.bgImage
+      ? await handleFileUpload(req.files.bgImage[0], "backgrounds")
+      : null;
+
+    // Parse theme
+    let theme = req.body.theme ? JSON.parse(req.body.theme) : null;
+    if (bgImage && theme) {
+      theme.bgImage = bgImage;
     }
 
-    // Retrieve the existing document
     const existingSite = await UserSite.findOne({ user: id });
     if (!existingSite) {
       return res.status(404).json({ message: "Site not found" });
     }
 
     const formattedSlug = slug?.replace(/\s+/g, "-").toLowerCase();
-
-    // Construct the new fields object
     const newFields = {
       slug: formattedSlug || slug || existingSite.slug,
       social: social ? JSON.parse(social) : existingSite.social,
@@ -167,17 +129,14 @@ const UserSiteController = {
       skills: skills ? JSON.parse(skills) : existingSite.skills,
     };
 
-    // Use lodash to compare and only keep changed fields
-    const fieldsToUpdate = _.omitBy(newFields, (value, key) => {
-      return _.isEqual(value, _.get(existingSite, key));
-    });
+    const fieldsToUpdate = _.omitBy(newFields, (value, key) =>
+      _.isEqual(value, _.get(existingSite, key))
+    );
 
-    // If there are no fields to update, return the existing document
     if (_.isEmpty(fieldsToUpdate)) {
       return res.json(existingSite);
     }
 
-    // Update the necessary fields
     const updatedSite = await UserSite.findOneAndUpdate(
       { user: id },
       { $set: fieldsToUpdate },
@@ -198,15 +157,13 @@ const UserSiteController = {
       },
     });
 
-    res.json(updatedSite);
+    return res.json(updatedSite);
   }),
+
   addLinks: tryCatch(async (req, res) => {
     const { url, title, type } = req.body;
     const id = req.cookies["id"];
     const io = req.app.get("io");
-    const site = await UserSite.findOne({ user: id });
-
-    // Extract domain from the url
 
     const extractDomain = (url) => {
       const pattern = /(?:https?:\/\/)?(?:www\.)?([^\/.:]+)/;
@@ -215,9 +172,6 @@ const UserSiteController = {
     };
 
     const domain = extractDomain(url);
-
-    // Create new Link : header or url
-
     const newLink = await Links.create({
       user: id,
       url: type !== "Header" && url,
@@ -225,23 +179,19 @@ const UserSiteController = {
       type: type,
       display: title == "" ? false : true,
     });
-    // Add new Link to user site
+
     const userSite = await UserSite.findOneAndUpdate(
       { user: id },
-      {
-        $push: {
-          links: newLink,
-        },
-      },
+      { $push: { links: newLink } },
       { new: true, useFindAndModify: false }
     ).populate({
       path: "links",
       match: { display: true },
     });
+
     await newLink.save();
     await userSite.save();
 
-    // Use ws to real-time data
     io.to(`user_${id}`).emit("site:update", {
       type: "SITE_UPDATE",
       payload: {
@@ -251,8 +201,9 @@ const UserSiteController = {
       },
     });
 
-    res.json(newLink);
+    return res.json(newLink);
   }),
+
   editLinks: tryCatch(async (req, res) => {
     const { url, title, index, display } = req.body;
     const id = req.cookies["id"];
@@ -262,16 +213,15 @@ const UserSiteController = {
       url,
       title,
       index,
+      ...(display !== undefined && { display }),
     };
-    if (display !== undefined) {
-      updateData.display = display;
-    }
 
     const updatedLink = await Links.findOneAndUpdate(
       { _id: req.params.id },
       updateData,
       { new: true }
     );
+
     if (!updatedLink) {
       return res.status(404).json({ message: "Link not found" });
     }
@@ -281,7 +231,6 @@ const UserSiteController = {
       match: { display: true },
     });
 
-    // Use ws to real-time data
     io.to(`user_${id}`).emit("site:update", {
       type: "SITE_UPDATE",
       payload: {
@@ -291,62 +240,45 @@ const UserSiteController = {
       },
     });
 
-    res.json(updatedLink);
+    return res.json(updatedLink);
   }),
+
   reorder: tryCatch(async (req, res) => {
     const id = req.cookies["id"];
     const io = req.app.get("io");
     const { links } = req.body;
 
-    console.log("Received links:", links);
-
-    // Find the user's site
     const userSite = await UserSite.findOne({ user: id });
-
     if (!userSite) {
-      return res
-        .status(404)
-        .json({ success: false, message: "User site not found" });
-    }
-
-    try {
-      // Create a new array of link IDs in the correct order
-      const newLinkOrder = links.map(
-        (link) => new mongoose.Types.ObjectId(link.id)
-      );
-
-      // Update the userSite document
-      userSite.links = newLinkOrder;
-
-      // Save the updated userSite
-      await userSite.save();
-
-      // Fetch the updated site with populated links
-      const updatedSite = await UserSite.findOne({ user: id }).populate(
-        "links"
-      );
-
-      io.to(`user_${id}`).emit("site:update", {
-        type: "SITE_UPDATE",
-        payload: {
-          reorder: newLinkOrder,
-          site: updatedSite,
-        },
-      });
-      res.json({
-        success: true,
-        message: "Order updated successfully",
-        site: updatedSite,
-      });
-    } catch (error) {
-      console.error("Error updating link order:", error);
-      res.status(500).json({
+      return res.status(404).json({
         success: false,
-        message: "Error updating link order",
-        error: error.message,
+        message: "User site not found",
       });
     }
+
+    const newLinkOrder = links.map(
+      (link) => new mongoose.Types.ObjectId(link.id)
+    );
+    userSite.links = newLinkOrder;
+    await userSite.save();
+
+    const updatedSite = await UserSite.findOne({ user: id }).populate("links");
+
+    io.to(`user_${id}`).emit("site:update", {
+      type: "SITE_UPDATE",
+      payload: {
+        reorder: newLinkOrder,
+        site: updatedSite,
+      },
+    });
+
+    return res.json({
+      success: true,
+      message: "Order updated successfully",
+      site: updatedSite,
+    });
   }),
+
   removeLinks: tryCatch(async (req, res) => {
     const user = req.cookies["id"];
     const { itemId } = req.body;
@@ -361,6 +293,10 @@ const UserSiteController = {
       match: { display: true },
     });
 
+    if (!userSite) {
+      return res.status(404).json({ message: "Item not found" });
+    }
+
     io.to(`user_${user}`).emit("site:update", {
       type: "SITE_UPDATE",
       payload: {
@@ -370,11 +306,7 @@ const UserSiteController = {
       },
     });
 
-    if (!userSite) {
-      return res.status(404).json({ message: "Item not found" });
-    }
-
-    res.status(200).json({ message: "Item removed successfully", userSite });
+    return res.json({ message: "Item removed successfully", userSite });
   }),
 };
 
--- src/middleware/middleware.mjs
+++ src/middleware/middleware.mjs
@@ -1,14 +1,43 @@
 import AppError from "../helpers/AppError.mjs";
 
-const errorHandler = (error, req, res, next) => {
-  if (error instanceof AppError) {
-    return res.status(error.statusCode).json({
-      errorCode: error.errorCode,
-      message: error.message,
+const errorHandler = (err, req, res, next) => {
+  // Check if headers have already been sent
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  // Log the error for debugging
+  console.error("Error:", err);
+
+  // Handle different types of errors
+  if (err.name === "ValidationError") {
+    return res.status(400).json({
+      status: "error",
+      message: "Validation Error",
+      details: err.message,
     });
   }
 
-  return res.status(500).send(error.message);
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).json({
+      status: "error",
+      message: "Unauthorized Access",
+    });
+  }
+
+  if (err.name === "NotFoundError") {
+    return res.status(404).json({
+      status: "error",
+      message: "Resource Not Found",
+    });
+  }
+
+  // Default error response
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+    ...(process.env.NODE_ENV === "development" && { details: err.message }),
+  });
 };
 
 export default errorHandler;
