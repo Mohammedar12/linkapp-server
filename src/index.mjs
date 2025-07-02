3285993a0a569ce437b969b70d0f484ea25e5528
Mohammedar12 <alikkar87@gmail.com>
2025-07-03 02:48:17 +0300

    update domain

--- src/index.mjs
+++ src/index.mjs
@@ -87,7 +87,7 @@ app.set("io", io);
 app.set("userSockets", userSockets);
 
 const corsOptions = {
-  origin: "https://wasl.iiali.com",
+  origin: process.env.ALLOWED_ORIGIN,
   credentials: true,
 };
 
